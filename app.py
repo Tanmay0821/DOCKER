c560e64a277f0e7388fe17d0010038eff8bba884
Tanmay Gavhane <tanmaygavhane8@gmail.com>
2025-12-28 11:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, this is my first Flask app!"
+@app.route('/')
+def home():
+    return "Hello from VM!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
